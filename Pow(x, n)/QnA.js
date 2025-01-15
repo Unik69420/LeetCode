a08771ecb41d3dae0fbced5739f9a20c131cb1d8
aman_sama <amanrai2002acr@gmail.com>
2025-01-15 20:34:01 +0545

    more optimized

--- Pow(x, n)/QnA.js
+++ Pow(x, n)/QnA.js
@@ -18,7 +18,15 @@
  * @param {number} x
  * @param {number} n
  * @return {number}
- */
-var myPow = function (x, n) {
-  return Math.pow(x, n);
+ */ var myPow = function (x, n) {
+  if (n === 0) return 1;
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+  if (n % 2 === 0) {
+    return myPow(x * x, n / 2);
+  } else {
+    return x * myPow(x, n - 1);
+  }
 };
